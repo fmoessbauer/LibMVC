4374f928c71b3edcdfdced11b71ce2c4e7a53307
Felix Moessbauer <felix.moessbauer@gmail.com>
2017-11-30 12:25:34 +0100

    Initial commit

---
+++ bench/fetch_graphs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "fetch set of common graphs"
+cd data
+wget -r -nd --no-parent -A '*-mis.tar.gz' "http://www.nlsde.buaa.edu.cn/~kexu/benchmarks/"
+
+echo "extract graphs"
+for file in $(find . -name "*.tar.gz"); do
+  tar -xf "$file"
+  rm "$file"
+done
