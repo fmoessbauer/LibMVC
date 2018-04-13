7ad0452c6ad6198a17a3062c16b9ba8c9832c185
Felix Moessbauer <felix.moessbauer@gmail.com>
2018-04-13 16:22:36 +0200

    add option to set static seed

--- bench/main.cpp
+++ bench/main.cpp
@@ -18,7 +18,8 @@ static void BM_Solver(
     benchmark::State& state,
     const std::string filename,
     const int coversize,
-    const std::chrono::seconds time)
+    const std::chrono::seconds time,
+    unsigned int seed)
 {
   int successes = 0;
   int min_cover = std::numeric_limits<int>::max();
@@ -29,9 +30,13 @@ static void BM_Solver(
       auto error = std::string("No such file: ") + filename;
       state.SkipWithError(error.c_str());
     }
+    // If no seed given, use a fresh seed per iteration
+    if(seed == 0){
+      seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    }
     state.ResumeTiming();
 
-    Solver solver(file, coversize, time);
+    Solver solver(file, coversize, time, false, seed);
     solver.cover_LS();
 
     state.PauseTiming();
@@ -46,9 +51,16 @@ static void BM_Solver(
 }
 
 int main(int argc, char** argv){
-  if(argc < 3){
-    std::cout << "usage: <filename> <coversize> <time(s)>" << std::endl;
+  benchmark::Initialize(&argc, argv);
+
+  if(argc < 3 || argc > 5){
+    std::cout << "usage: <filename> <coversize> <time(s)> [<seed>]" << std::endl;
     return 1;
+  }
+
+  unsigned int seed = 0;
+  if(argc == 5) {
+    seed = std::stoi(argv[4]);
   }
 
   std::string filepath(argv[1]);
@@ -64,27 +76,25 @@ int main(int argc, char** argv){
       + std::to_string(coversize) + "/" 
       + std::to_string(time_limit.count()));
 
-  benchmark::Initialize(&argc, argv);
-
   // Register benchmarks
   benchmark::RegisterBenchmark(
       (std::string("NuMVC/") + config).c_str(), [&](benchmark::State & st){
-          return BM_Solver<NuMVC>(st, filepath, coversize, time_limit);
+          return BM_Solver<NuMVC>(st, filepath, coversize, time_limit, seed);
         })->Unit(benchmark::kMillisecond);
 
   benchmark::RegisterBenchmark(
       (std::string("FastVC/") + config).c_str(), [&](benchmark::State & st){
-          return BM_Solver<FastVC>(st, filepath, coversize, time_limit);
+          return BM_Solver<FastVC>(st, filepath, coversize, time_limit, seed);
         })->Unit(benchmark::kMillisecond);
 
   benchmark::RegisterBenchmark(
       (std::string("ParallelSolverAdapter<NuMVC>/") + config).c_str(), [&](benchmark::State & st){
-          return BM_Solver<ParallelSolverAdapter<NuMVC>>(st, filepath, coversize, time_limit);
+          return BM_Solver<ParallelSolverAdapter<NuMVC>>(st, filepath, coversize, time_limit, seed);
         })->Unit(benchmark::kMillisecond);
 
   benchmark::RegisterBenchmark(
       (std::string("ParallelSolverAdapter<FastVC>/") + config).c_str(), [&](benchmark::State & st){
-          return BM_Solver<ParallelSolverAdapter<FastVC>>(st, filepath, coversize, time_limit);
+          return BM_Solver<ParallelSolverAdapter<FastVC>>(st, filepath, coversize, time_limit, seed);
         })->Unit(benchmark::kMillisecond);
 
   benchmark::RunSpecifiedBenchmarks();
